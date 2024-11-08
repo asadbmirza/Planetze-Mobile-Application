fcb1c0ed958838829c00f453186be8e184b18004
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-07 22:26:26 -0500

    feat: created login logic

--- app/src/main/java/com/example/plantezemobileapplication/LoginActivity.java
+++ app/src/main/java/com/example/plantezemobileapplication/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.plantezemobileapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.plantezemobileapplication.presenter.LoginPresenter;
+import com.example.plantezemobileapplication.view.LoginView;
+import com.google.android.material.textfield.TextInputEditText;
+
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity implements LoginView {
+
+    TextInputEditText emailText, passwordText;
+    Button logInBtn;
+    private LoginPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,39 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        presenter = new LoginPresenter(this);
+
+        emailText = findViewById(R.id.email);
+        passwordText = findViewById(R.id.password);
+        logInBtn = findViewById(R.id.login_btn);
+
+        logInBtn.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(emailText.getText()).toString();
+            String password = Objects.requireNonNull(passwordText.getText()).toString();
+
+            presenter.loginUser(email, password);
+        });
+    }
+
+
+    @Override
+    public void showLoginSuccess() {
+        Toast.makeText(this, "Logged in.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showLoginFailure() {
+        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showLoading() {
+        // Show a loading spinner or progress bar
+    }
+
+    @Override
+    public void hideLoading() {
+        // Hide the loading spinner or progress bar
     }
 }
---
+++ app/src/main/java/com/example/plantezemobileapplication/view/LoginView.java
@@ -0,0 +1,8 @@
+package com.example.plantezemobileapplication.view;
+
+public interface LoginView {
+    void showLoginSuccess();
+    void showLoginFailure();
+    void showLoading();
+    void hideLoading();
+}
