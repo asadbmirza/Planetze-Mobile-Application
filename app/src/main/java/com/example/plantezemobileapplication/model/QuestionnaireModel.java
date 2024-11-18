53ace2d073c79adb69726afb30fdd46b5f3beb85
Jeremy Chong <chong.jeremy@hotmail.com>
2024-11-17 23:14:13 -0500

    chore: add helper classes to promote mvp project structure

---
+++ app/src/main/java/com/example/plantezemobileapplication/model/QuestionnaireModel.java
@@ -0,0 +1,35 @@
+package com.example.plantezemobileapplication.model;
+
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionnaireModel {
+    FirebaseAuth auth;
+
+    public QuestionnaireModel() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void saveQuestionnaireInfo(Map<String, Double> categoryEmissions, String userCountry) {
+        FirebaseUser currUser = auth.getCurrentUser();
+        String userId = "";
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        if (currUser != null) {
+            userId = currUser.getUid();
+        }
+        else {
+            userId = "hRGBz0zBIGRbm6wJm9RA5Jii97M2";
+        }
+        ref.child("users").child(userId).child("annualEmissions").setValue(categoryEmissions);
+        ref.child("users").child(userId).child("country").setValue(userCountry);
+    }
+}
---
+++ app/src/main/java/com/example/plantezemobileapplication/presenter/QuestionnaireResultPresenter.java
@@ -0,0 +1,31 @@
+package com.example.plantezemobileapplication.presenter;
+
+import com.example.plantezemobileapplication.model.QuestionnaireModel;
+import com.example.plantezemobileapplication.utils.JsonParser;
+import com.example.plantezemobileapplication.view.questionnaire.QuestionnaireResultActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionnaireResultPresenter {
+    private QuestionnaireModel model;
+    private QuestionnaireResultActivity view;
+
+    public QuestionnaireResultPresenter() {}
+
+    public QuestionnaireResultPresenter(QuestionnaireModel model, QuestionnaireResultActivity view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setTotalEmissionDisplay(Map<String, Double> totalEmissions, String country) {
+        JsonParser jsonParser = new JsonParser(view);
+        double countryEmissions = jsonParser.getEmissionByCountry("countryEmissions.json", country);
+        view.setUserTotalText(String.format("%.3g%n", totalEmissions.get("Total") * 0.001));
+        view.setCountryEmissions(String.format("%.3g%n", countryEmissions));
+    }
+
+    public void uploadUserResults(Map<String, Double> totalEmissions, String country) {
+        model.saveQuestionnaireInfo(totalEmissions, country);
+    }
+}
