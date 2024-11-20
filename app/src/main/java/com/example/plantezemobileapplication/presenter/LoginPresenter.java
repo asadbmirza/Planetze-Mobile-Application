38a769fd5e2e39259878d18abaa8cc1767ac617a
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-19 23:37:20 -0500

    refactor: refactor login page to use MVP

--- app/src/main/java/com/example/plantezemobileapplication/presenter/LoginPresenter.java
+++ app/src/main/java/com/example/plantezemobileapplication/presenter/LoginPresenter.java
@@ -2,20 +2,19 @@ package com.example.plantezemobileapplication.presenter;
 
 import androidx.annotation.NonNull;
 
+import com.example.plantezemobileapplication.model.LoginModel;
 import com.example.plantezemobileapplication.view.login.ProcessView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPresenter {
     private final ProcessView view;
-    private final FirebaseAuth mAuth;
+    private final LoginModel loginModel;
 
     public LoginPresenter(ProcessView view) {
         this.view = view;
-        mAuth = FirebaseAuth.getInstance();
+        this.loginModel = new LoginModel();
     }
 
     public void loginUser(String email, String password) {
@@ -23,25 +22,25 @@ public class LoginPresenter {
             view.showLoading();
         }
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (view != null) {
-                            view.hideLoading();
-                        }
-                        if (task.isSuccessful()) {
-                            verifyUser();
-                        } else {
-                            view.showProcessFailure("Authentication failed.");
-                        }
+        loginModel.loginUser(email, password, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (view != null) {
+                    view.hideLoading();
+
+                    if (task.isSuccessful()) {
+                        verifyUser();
+                    } else {
+                        view.showProcessFailure("Authentication failed.");
                     }
-                });
+                }
+
+            }
+        });
     }
 
     private void verifyUser() {
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null && user.isEmailVerified()) {
+        if (loginModel.isVerified()) {
             view.showProcessSuccess("Logged in.");
         } else {
             view.showProcessFailure("Please verify your email before logging in.");
