3c36c105a1fdf3a82e5df8018e3bf1ed681f6d56
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-20 21:11:22 -0500

    feat: added login page tests using mockito framework

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
