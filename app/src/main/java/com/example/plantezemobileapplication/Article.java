1931fd7ac238ed72b7a458bf4b761238af5e633f
Moiz <mohammedmoiz176@gmail.com>
2024-11-20 23:02:15 -0500

    feat: modified to implement articles page allowing program to recieve articles data from News API

--- app/src/main/java/com/example/plantezemobileapplication/Article.java
+++ app/src/main/java/com/example/plantezemobileapplication/Article.java
@@ -1,4 +1,29 @@
 package com.example.plantezemobileapplication;
 
 public class Article {
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
 }
--- app/src/main/java/com/example/plantezemobileapplication/NewsApiService.java
+++ app/src/main/java/com/example/plantezemobileapplication/NewsApiService.java
@@ -1,4 +1,9 @@
 package com.example.plantezemobileapplication;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 public interface NewsApiService {
+    @GET("v2/everything")
+    Call<NewsResponse> getNews(@Query("q") String query, @Query("language") String language, @Query("apiKey") String apiKey);
 }
