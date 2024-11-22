349abfc10319a06b0b53b516131f645f6dd9f17d
Jeremy Chong <chong.jeremy@hotmail.com>
2024-11-21 20:01:50 -0500

    feat: add activity log list for daily quizzes; add boiler code for activity list; test data transfer between Eco-tracker monitor and activity list

--- app/src/main/java/com/example/plantezemobileapplication/view/ActivityListFragment.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/ActivityListFragment.java
@@ -3,15 +3,14 @@ package com.example.plantezemobileapplication.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.plantezemobileapplication.R;
-import com.example.plantezemobileapplication.utils.Answer;
 import com.example.plantezemobileapplication.utils.Question;
 
 import java.util.ArrayList;
@@ -19,21 +18,23 @@ import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EcoTrackerMonitorFragment#newInstance} factory method to
+ * Use the {@link ActivityListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EcoTrackerMonitorFragment extends Fragment {
+public class ActivityListFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_QUESTIONS = "questions";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private List<Question> questions;
 
-    public EcoTrackerMonitorFragment() {
+    public ActivityListFragment() {
         // Required empty public constructor
     }
 
@@ -43,14 +44,22 @@ public class EcoTrackerMonitorFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EcoTrackerMonitorFragment.
+     * @return A new instance of fragment ActivityListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EcoTrackerMonitorFragment newInstance(String param1, String param2) {
-        EcoTrackerMonitorFragment fragment = new EcoTrackerMonitorFragment();
+    public static ActivityListFragment newInstance(String param1, String param2) {
+        ActivityListFragment fragment = new ActivityListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static ActivityListFragment newInstance(List<Question> questions) {
+        ActivityListFragment fragment = new ActivityListFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARG_QUESTIONS, (ArrayList<? extends Parcelable>) questions);
         fragment.setArguments(args);
         return fragment;
     }
@@ -61,30 +70,17 @@ public class EcoTrackerMonitorFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            questions = getArguments().getParcelableArrayList(ARG_QUESTIONS);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_eco_tracker_monitor, container, false);;
-        Button btn = view.findViewById(R.id.button5);
-
-        //Redirect user to specific activity log
-        btn.setOnClickListener(v -> {
-            Answer[] answers = {new Answer("Answer1", 2)};
-            List<Question> questionList = new ArrayList<>();
-            questionList.add(new Question("What is your favourite color?", answers,"Food"));
-            Fragment targetFragment = ActivityListFragment.newInstance(questionList);
-            FragmentTransaction transaction = requireActivity()
-                    .getSupportFragmentManager()
-                    .beginTransaction();
-
-            transaction.replace(R.id.fragmentContainerView, targetFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        });
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_activity_list, container, false);
+        TextView text = view.findViewById(R.id.blah);
+        text.setText(questions.get(0).getTitle() + " " + questions.get(0).getCategory() + " " + questions.get(0).getAnswers()[0].getAnswerText());
         return view;
     }
 }
