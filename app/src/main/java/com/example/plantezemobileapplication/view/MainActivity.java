a8040054fa65da8b037b38aaa4afa24866587a2d
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-24 00:24:07 -0500

    feat: implemented navbar fragment switching logic

--- app/src/main/java/com/example/plantezemobileapplication/view/MainActivity.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.plantezemobileapplication.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,26 +13,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.plantezemobileapplication.R;
 import com.example.plantezemobileapplication.view.login.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         if (auth.getCurrentUser() == null) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
             finish();
         }
+
+        if (savedInstanceState == null) {
+            loadFragment(new EcoGaugeFragment());
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment;
+                if (item.getItemId() == R.id.ecoGauge) {
+                    selectedFragment = new EcoGaugeFragment();
+                } else if (item.getItemId() == R.id.ecoBalance) {
+                    selectedFragment = new EcoGaugeFragment();
+                } else if (item.getItemId() == R.id.ecoHub) {
+                    selectedFragment = new EcoGaugeFragment();
+                } else if (item.getItemId() == R.id.ecoTracker) {
+                    selectedFragment = new EcoGaugeFragment();
+                } else {
+                    selectedFragment = null;
+                }
+                return loadFragment(selectedFragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
