0cd50b3d74b2b24aeadbb2a1eae972e410ec2b91
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-25 20:22:25 -0500

    feat: updated reset pass page to match login/register pages

--- app/src/main/java/com/example/plantezemobileapplication/view/login/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/login/ForgotPasswordActivity.java
@@ -34,7 +34,7 @@ public class ForgotPasswordActivity extends AppCompatActivity implements Process
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.reset_pass_page), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -72,6 +72,7 @@ public class ForgotPasswordActivity extends AppCompatActivity implements Process
     @Override
     public void showLoading() {
         // Show a loading spinner or progress bar
+        reset_pass_btn.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
     }
 
@@ -79,5 +80,6 @@ public class ForgotPasswordActivity extends AppCompatActivity implements Process
     public void hideLoading() {
         // Hide the loading spinner or progress bar
         progressBar.setVisibility(View.GONE);
+        reset_pass_btn.setVisibility(View.VISIBLE);
     }
 }
