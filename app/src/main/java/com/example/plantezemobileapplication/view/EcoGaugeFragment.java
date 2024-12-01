f266dbb3d5db83827fba370322d39c8eb6bd667c
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-12-01 16:13:06 -0500

    feat: added functionality to emissions trend buttons

--- app/src/main/java/com/example/plantezemobileapplication/view/EcoGaugeFragment.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/EcoGaugeFragment.java
@@ -10,9 +10,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.plantezemobileapplication.Emissions;
 import com.example.plantezemobileapplication.R;
+import com.example.plantezemobileapplication.User;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -24,17 +26,25 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 public class EcoGaugeFragment extends Fragment {
 
     PieChart emissionsBreakdownPieChart;
-    LineChart emissionsTrendLineChart;
-    static Emissions emissions;
+    static LineChart emissionsTrendLineChart;
+    Button dailyTrendBtn, weeklyTrendBtn, monthlyTrendBtn;
+    static User user;
+    static Emissions annualEmissions;
 
     public EcoGaugeFragment() {
         // Required empty public constructor
@@ -46,15 +56,31 @@ public class EcoGaugeFragment extends Fragment {
 
         emissionsBreakdownPieChart = view.findViewById(R.id.emission_breakdown_chart);
         emissionsTrendLineChart = view.findViewById(R.id.emissions_trend_chart);
+        dailyTrendBtn = view.findViewById(R.id.daily_trend_btn);
+        weeklyTrendBtn = view.findViewById(R.id.weekly_trend_btn);
+        monthlyTrendBtn = view.findViewById(R.id.monthly_trend_btn);
+
+        dailyTrendBtn.setOnClickListener(v -> {
+            renderEmissionsTrend(getDailyEmissionTrendDataSet());
+        });
+
+        weeklyTrendBtn.setOnClickListener(v -> {
+            renderEmissionsTrend(getWeeklyEmissionTrendDataSet());
+        });
+
+        monthlyTrendBtn.setOnClickListener(v -> {
+            renderEmissionsTrend(getMonthlyEmissionTrendDataSet());
+        });
 
         if (getArguments() != null) {
-            emissions = (Emissions) getArguments().getSerializable("emissions_key");
-            if(emissions == null) {
+            user = (User) getArguments().getSerializable("user_key");
+            annualEmissions = user.getAnnualEmissions();
+            if(annualEmissions == null) {
                 Log.e("EmissionsRetrieval", "Emissions is null.");
-                emissions = new Emissions();
+                user.setAnnualEmissions(new Emissions());
             }
             renderEmissionsBreakdown();
-            renderEmissionsTrend();
+            renderEmissionsTrend(getDailyEmissionTrendDataSet());
         } else {
             Log.e("EmissionsRetrieval", "Failed to get emissions");
         }
@@ -63,9 +89,9 @@ public class EcoGaugeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (emissions != null) {
+        if (annualEmissions != null) {
             renderEmissionsBreakdown();
-            renderEmissionsTrend();
+            renderEmissionsTrend(getDailyEmissionTrendDataSet());
         } else {
             Log.e("EmissionsRetrieval", "Emissions is null in onResume.");
         }
@@ -103,23 +129,22 @@ public class EcoGaugeFragment extends Fragment {
     private static PieDataSet getEmissionsBreakdownDataSet() {
         ArrayList<PieEntry> entries = new ArrayList<>();
 
-        entries.add(new PieEntry(emissions.getTransportation(), "Transportation"));
-        entries.add(new PieEntry(emissions.getConsumption(), "Consumption"));
-        entries.add(new PieEntry(emissions.getFood(), "Food"));
-        entries.add(new PieEntry(emissions.getHousing(), "Housing"));
+        entries.add(new PieEntry(annualEmissions.getTransportation(), "Transportation"));
+        entries.add(new PieEntry(annualEmissions.getConsumption(), "Consumption"));
+        entries.add(new PieEntry(annualEmissions.getFood(), "Food"));
+        entries.add(new PieEntry(annualEmissions.getHousing(), "Housing"));
 
         return new PieDataSet(entries, "");
     }
 
-    private void renderEmissionsTrend() {
-        LineDataSet setComp1 = getEmissionsTrendDataSet();
-        setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        setComp1.setLineWidth(5);
-        setComp1.setCircleRadius(6);
-        setComp1.setColor(Color.rgb(0,153,153));
+    private void renderEmissionsTrend(LineDataSet data) {
+        data.setAxisDependency(YAxis.AxisDependency.LEFT);
+        data.setLineWidth(5);
+        data.setCircleRadius(6);
+        data.setColor(Color.rgb(0,153,153));
 
         List<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(setComp1);
+        dataSets.add(data);
 
         LineData lineData = new LineData(dataSets);
         lineData.setValueTextColor(Color.BLACK);
@@ -130,6 +155,7 @@ public class EcoGaugeFragment extends Fragment {
 
         emissionsTrendLineChart.setData(lineData);
         emissionsTrendLineChart.setDrawGridBackground(false);
+        emissionsTrendLineChart.setExtraOffsets(50f, 0f, 50f, 0f);
 
         // Simplify chart appearance
         emissionsTrendLineChart.setDescription(null);
@@ -140,23 +166,96 @@ public class EcoGaugeFragment extends Fragment {
         emissionsTrendLineChart.getAxisLeft().setDrawLabels(false);
         emissionsTrendLineChart.getAxisRight().setDrawGridLines(false);
         emissionsTrendLineChart.getAxisRight().setDrawLabels(false);
-        emissionsTrendLineChart.getXAxis().setDrawGridLines(false);
 
-        emissionsTrendLineChart.getXAxis().setGranularity(1f); // Force whole number increments
-        emissionsTrendLineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        XAxis xAxis = emissionsTrendLineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // Position the X-axis at the bottom
+        xAxis.setGranularity(1f); // Ensure values are displayed at regular intervals
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularityEnabled(true);
 
         emissionsTrendLineChart.animateY(500);
         emissionsTrendLineChart.invalidate();
     }
 
-    @NonNull
-    private static LineDataSet getEmissionsTrendDataSet() {
+    private static LineDataSet getDailyEmissionTrendDataSet() {
         List<Entry> emissions = new ArrayList<>();
+        HashMap<String, Emissions> dailyEmissions = user.getDailyEmissions();
 
-        emissions.add(new Entry(0f, 100000f));
-        emissions.add(new Entry(1f, 140000f));
-        emissions.add(new Entry(2f, 160000f));
-        emissions.add(new Entry(3f, 180000f));
+        if (dailyEmissions != null) {
+            List<String> dailyDates = new ArrayList<>(dailyEmissions.keySet());
+            Collections.sort(dailyDates);
+            int index = 0;
+
+            for (String date : dailyDates) {
+                emissions.add(new Entry(index, dailyEmissions.get(date).getTotal()));
+                index++;
+            }
+
+            // Formats x-axis labels to shown only the month and date of each log
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd", Locale.getDefault());
+            XAxis xAxis = emissionsTrendLineChart.getXAxis();
+            xAxis.setValueFormatter(new ValueFormatter() {
+                @Override
+                public String getFormattedValue(float value) {
+                    int index = (int) value;
+                    try {
+                        Date date = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA).parse(dailyDates.get(index));
+                        return dateFormat.format(date);
+                    } catch (Exception e) {
+                        return dailyDates.get(index);
+                    }
+                }
+            });
+        }
+
+        return new LineDataSet(emissions, "");
+    }
+
+    private static LineDataSet getWeeklyEmissionTrendDataSet() {
+        List<Entry> emissions = new ArrayList<>();
+        HashMap<String, Emissions> weeklyEmissions = user.getWeeklyEmissions();
+
+        if (weeklyEmissions != null) {
+            List<String> weeklyDates = new ArrayList<>(weeklyEmissions.keySet());
+            Collections.sort(weeklyDates);
+            int index = 0;
+
+            for (String date : weeklyDates) {
+                emissions.add(new Entry(index, weeklyEmissions.get(date).getTotal()));
+                index++;
+            }
+
+            XAxis xAxis = emissionsTrendLineChart.getXAxis();
+            xAxis.setValueFormatter(new ValueFormatter() {
+                @Override
+                public String getFormattedValue(float value) {
+                    int index = (int) value;
+                    if (index >= 0 && index < weeklyDates.size()) {
+                        return weeklyDates.get(index);
+                    } else {
+                        return "";
+                    }
+                }
+            });
+        }
+
+        return new LineDataSet(emissions, "");
+    }
+
+    private static LineDataSet getMonthlyEmissionTrendDataSet() {
+        List<Entry> emissions = new ArrayList<>();
+        HashMap<String, Emissions> monthlyEmissions = user.getMonthlyEmissions();
+
+        if (monthlyEmissions != null) {
+            List<String> monthlyDates = new ArrayList<>(monthlyEmissions.keySet());
+            Collections.sort(monthlyDates);
+            int index = 0;
+
+            for (String date : monthlyDates) {
+                emissions.add(new Entry(index, monthlyEmissions.get(date).getTotal()));
+                index++;
+            }
+        }
 
         return new LineDataSet(emissions, "");
     }
