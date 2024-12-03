4d4b393e8a9b3b20e39bc9781dc29437bcf27b4f
Moiz <mohammedmoiz176@gmail.com>
2024-12-02 22:49:51 -0500

    refactor: Refactored code to adhere to MVP format

--- app/src/main/java/com/example/plantezemobileapplication/view/EcoHub/EcoHubFragment.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/EcoHub/EcoHubFragment.java
@@ -1,4 +1,4 @@
-package com.example.plantezemobileapplication.view;
+package com.example.plantezemobileapplication.view.EcoHub;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
--- app/src/main/java/com/example/plantezemobileapplication/view/EcoHub/EcoHubView.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/EcoHub/EcoHubView.java
@@ -1,4 +1,4 @@
-package com.example.plantezemobileapplication.view;
+package com.example.plantezemobileapplication.view.EcoHub;
 
 import androidx.fragment.app.Fragment;
 
