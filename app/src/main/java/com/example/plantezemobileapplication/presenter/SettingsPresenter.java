de888c34113736e008f265c04e09d676f8e6ab96
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-12-03 18:22:35 -0500

    feat: started edit user implementation

--- app/src/main/java/com/example/plantezemobileapplication/presenter/SettingsPresenter.java
+++ app/src/main/java/com/example/plantezemobileapplication/presenter/SettingsPresenter.java
@@ -1,4 +1,14 @@
 package com.example.plantezemobileapplication.presenter;
 
+import com.example.plantezemobileapplication.model.SettingsModel;
+import com.example.plantezemobileapplication.view.settings.SettingsView;
+
 public class SettingsPresenter {
+    SettingsView view;
+    SettingsModel model;
+
+    public SettingsPresenter(SettingsView view, SettingsModel model) {
+        this.view = view;
+        this.model = model;
+    }
 }
