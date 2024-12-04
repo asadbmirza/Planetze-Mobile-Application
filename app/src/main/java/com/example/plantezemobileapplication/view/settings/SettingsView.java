21b2ee1a428fa1df22a6747d070bf84480eb5ee6
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-12-03 20:48:33 -0500

    feat: created settings view

--- app/src/main/java/com/example/plantezemobileapplication/view/settings/SettingsView.java
+++ app/src/main/java/com/example/plantezemobileapplication/view/settings/SettingsView.java
@@ -1,4 +1,6 @@
 package com.example.plantezemobileapplication.view.settings;
 
 public interface SettingsView {
+    void showProcessSuccess(String message);
+    void showProcessFailure(String message);
 }
